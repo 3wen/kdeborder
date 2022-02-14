cb324c3a2963a289735e4af88feb629be9f5a86c
Ewen Gallic <ewengallic@iMac-de-Ewen.local>
2022-02-14 13:53:29 +0100

    Removed denpendency to rgeos

---
+++ R/kdeborder.R
@@ -0,0 +1,13 @@
+#' Kernel density estimation correcting for border bias
+#'
+#' This package proposes some R codes to compute the kernel density estimates of
+#' two-dimensional data points, using an extension of Ripley's circumference
+#' method to correct for border bias.
+#'
+#'
+#' @docType package
+#'
+#' @author Arthur Charpentier and Ewen Gallic \email{ewen.gallic@gmail.com}
+#'
+#' @name kdeborder
+NULL
